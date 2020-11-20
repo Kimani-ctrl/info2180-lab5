4040acc7888fb9f5beca2ef664aa61b1890bdfe8
Kimani Allen <kimani2761469@hotmail.com>
2020-11-20 01:01:52 -0500

    Final

--- world.php
+++ world.php
@@ -6,24 +6,57 @@ $dbname = 'world';
 $cname = $_GET['country'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$cname%'");
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
-<table width = 600px;>
-  <tr>
-    <th>Name</th>
-    <th>Continent</th>
-    <th>Independence</th>
-    <th>Head Of State</th>
-</tr>
-<?php foreach ($results as $row): ?>
-  <tr>
-  <td><?= $row['name']; ?></td> 
-  <td><?= $row['continent']; ?></td>
-  <td><?= $row['independence_year']; ?></td>
-  <td><?= $row['head_of_state']; ?><td>
-</tr>
-<?php endforeach; ?>
-</table>
+
+<?php if(isset($cname) && isset($_GET['context'])){
+ 
+  $stmt = $conn->query("SELECT * FROM countries JOIN cities ON cities.country_code=countries.code WHERE countries.name LIKE '%$cname%'");
+  // $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$cname%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);?>
+
+  <table>
+    <tr>
+      <th>name</th>
+      <th>District</th>
+      <th>Population</th>
+    </tr>
+    <?php foreach($results as $row){?>
+      <tr>
+        <td><?= $row['name']?></td>
+        <td><?= $row['district']?></td>
+        <td><?= $row['population']?></td>
+      </tr>
+    <?php } ?>
+  </table>
+  <?php } ?>
+
+  <?php if(isset($cname) && !isset($_GET['context'])){
+ 
+//  $stmt = $conn->query("SELECT * FROM countries JOIN cities ON cities.country_code=countries.code WHERE countries.name LIKE '%$cname%'");
+ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$cname%'");
+ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);?>
+
+ <table>
+   <tr>
+     <th>name</th>
+     <th>Continent</th>
+     <th>Independence</th>
+     <th>Head of State</th>
+   </tr>
+   <?php foreach($results as $row){?>
+     <tr>
+       <td><?= $row['name']?></td>
+       <td><?= $row['continent']?></td>
+       <td><?= $row['independence_year']?></td>
+       <td><?= $row['head_of_state']?></td>
+     </tr>
+   <?php } ?>
+ </table>
+ <?php } ?>
+
+ 
+
+
+
